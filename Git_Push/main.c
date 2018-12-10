cf6e721e92400f13e7ec209c13c1c16b08a334e7
Kamal Maged <Kamal_Maged@yahoo.com>
2018-12-10 17:48:35 -0500

    Initial Commit

---
+++ Git_Push/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Git_Push
+//
+//  Created by Kamal Maged on 2018-12-10.
+//  Copyright © 2018 Kamal Maged. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
